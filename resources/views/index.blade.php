e11a45ea74f938133d4fadaf4b03a99d0e784b0f
Mukti <mukti.devmail@gmail.com>
2023-09-15 16:33:22 +0700

    Fix tampilan index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -69,10 +69,10 @@
             <img src="{{ asset('img/freighter.jpg') }}" class="bg-img">
             <div class="container-fluid cover-hero">
                 <div class="row">
-                    <div class="col-sm-12 col-md-6">
+                    <div class="col-sm-12 col-md-6 text-center text-md-start">
                         <div class="row">
                             <div class="col">
-                                <h1>PT. Interbenua Logistindo</h1>
+                                <h1 class="company-name">PT. Interbenua Logistindo</h1>
                             </div>
                         </div>
                         <div class="row">
@@ -82,7 +82,7 @@
                         </div>
                         <div class="row">
                             <div class="col">
-                                <button type="button" class="btn btn-socmed" data-bs-toggle="modal" data-bs-target="#modalId">
+                                <button type="button" class="btn btn-socmed about-btn" data-bs-toggle="modal" data-bs-target="#modalId">
                                     About Us
                                 </button>
                             </div>
@@ -90,15 +90,18 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-12 col-md-12 text-end pb-2 pe-2 socmed-area" style="margin-bottom: 20px; margin-right: 20px;">
-                        <a href="https://api.whatsapp.com/send?phone=123456789" class="btn btn-socmed">
-                            <i class="fab fa-whatsapp" aria-hidden="true"></i> Whatsapp
-                        </a>
-                        <a href="mailto:email@example.com" class="btn btn-socmed">
-                            <i class="far fa-envelope" aria-hidden="true"></i> Email
-                        </a>
+                    <div class="col-sm-12 col-md-12 position-relative" style="margin-bottom: 20px; margin-right: 20px; padding-top: 6%;">
+                        <div class="position-absolute bottom-0 end-0 d-flex justify-content-center justify-content-md-end">
+                            <a href="https://api.whatsapp.com/send?phone=123456789" class="btn btn-socmed me-2 me-md-0 mb-2 mb-md-0">
+                                <i class="fab fa-whatsapp" aria-hidden="true"></i> Whatsapp
+                            </a>
+                            <a href="mailto:email@example.com" class="btn btn-socmed">
+                                <i class="far fa-envelope" aria-hidden="true"></i> Email
+                            </a>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
        </div>
