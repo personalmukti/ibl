4af6b1be61d8289f6cf1d31c29e374223a37949e
Mukti <personal.mukti@gmail.com>
2023-09-17 18:13:38 +0700

    fix edit

--- resources/views/partials/_connect.blade.php
+++ resources/views/partials/_connect.blade.php
@@ -4,12 +4,12 @@
         <div class="row">
             <div class="col-md-12">
                 <!-- Tombol WhatsApp -->
-                <a href="https://wa.me/your-phonenumber" class="btn btn-whatsapp float-md-right mb-3 mb-md-0">
+                <a href="https://wa.me/your-phonenumber" class="btn btn-whatsapp float-md-right mb-3 mb-md-0 btn-socmed ">
                     WhatsApp
                 </a>
 
                 <!-- Tombol Email -->
-                <a href="mailto:your-email@example.com" class="btn btn-email float-md-right">
+                <a href="mailto:your-email@example.com" class="btn btn-email float-md-right btn-socmed ">
                     Email
                 </a>
             </div>
